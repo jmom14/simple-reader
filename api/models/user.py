182bda713dc2f33d1d4667e3445b39a184a48ae8
Jose Ortega <jo.manuel.ortega@gmail.com>
2024-03-04 12:57:36 -0600

    Remove username and add password to User model

--- api/models/user.py
+++ api/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from typing import Union
 from . import Base
 
@@ -7,10 +7,8 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    password = Column(String)
     email = Column(String, unique=True, index=True)
     first_name = Column(String)
     last_name = Column(String)
-    is_disabled: Union[bool, None] = None
-
-
+    is_disabled = Column(Boolean, default=True)
--- api/schemas/user.py
+++ api/schemas/user.py
@@ -4,13 +4,15 @@ from typing import Union
 
 class User(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-
     id: int
-    username: str
     email: str
-    first_name: Union[str, None] = None
-    last_name: Union[str, None] = None
-    is_active = Union[str, None] = True
+    first_name: str
+    last_name: str
+    is_disabled: bool
 
 class UserPrivate(User):
     hashed_password: str
+
+
+class UserCreate(BaseModel):
+    pass
